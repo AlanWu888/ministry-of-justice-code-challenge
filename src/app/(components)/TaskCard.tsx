dcaec6f4f0dece2085217160d3fd67848cd891ca
“Alan <alan.wu@ibm.com>
2025-06-11 15:27:42 +0100

    styling and removed commented code

--- src/app/(components)/TaskCard.tsx
+++ src/app/(components)/TaskCard.tsx
@@ -20,6 +20,7 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
         border: "1px solid black",
         borderRadius: "10px",
         padding: "10px",
+        backgroundColor: "white"
       }}
     >
       <div
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-// import TaskCard from "./(components)/TaskCard";
 import TaskBoard from "./(components)/TaskBoard";
-import { Task } from "@/types/task";
 
 export default function Home() {
-  // const task: Task = {
-  //   id: 1,
-  //   title: "Visit the shops",
-  //   description: "Milk, eggs, bread",
-  //   status: "TODO",
-  //   dueDate: "2025-06-15T10:00:00.000Z",
-  //   createdAt: "2025-06-09T14:09:30.507Z",
-  //   updatedAt: "2025-06-09T14:09:30.507Z"
-  // };
 
   return (
     <div>
