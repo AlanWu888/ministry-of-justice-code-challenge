ca5c3930134701ab1afd6a36e27d4607c856366b
“Alan <alan.wu@ibm.com>
2025-06-23 19:16:33 +0100

    updated delete route to add guardrails

--- src/app/api/tasks/[id]/delete/route.ts
+++ src/app/api/tasks/[id]/delete/route.ts
@@ -2,12 +2,22 @@ import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 import { isAuthorised, UNAUTHORISED } from '@/utils/api';
 
-  export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    if (!isAuthorised(req)) return UNAUTHORISED
-  
-    await prisma.task.delete({
-      where: { id: parseInt(params.id) },
-    })
-  
-    return NextResponse.json({ message: 'Task deleted' })
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  if (!isAuthorised(req)) return UNAUTHORISED;
+
+  const id = parseInt(params.id);
+
+  const task = await prisma.task.findUnique({ where: { id } });
+
+  if (!task) {
+    return NextResponse.json({ error: 'Task not found' }, { status: 404 });
   }
+
+  if (!task.archived) {
+    return NextResponse.json({ error: 'Only archived tasks can be deleted' }, { status: 403 });
+  }
+
+  await prisma.task.delete({ where: { id } });
+
+  return NextResponse.json({ message: 'Task deleted' });
+}
